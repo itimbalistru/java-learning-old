18a220aa5c756d7280e6102f1d2293e33277dd72
timbalistru1990 <anisoara4>
2018-06-21 09:24:51 +0300

    First commit

---
+++ src/BeerSong.java
@@ -0,0 +1,24 @@
+public class BeerSong {
+    public static void main(String[] args) {
+        int beer = 99;
+        String word;
+        while (beer > 0) {
+            if (beer % 10 == 1) {
+                word = " sticla ";
+            } else {
+                word = "sticle";
+            }
+
+            System.out.println(beer + " " + word + " bere.");
+            System.out.println(" Ia una transmiteo la toti ");
+
+            beer = beer - 1;
+
+            if (beer > 0) {
+                System.out.println(beer + " " + word + " berea pe raft ");
+            } else {
+                System.out.println(" nu sunt sticle pe raft ");
+            }
+        }
+    }
+}
